d941d616e84beb0e3737231dbafdd914c9b79b3f
raviteja vaidya <vydyatej@gmail.com>
2026-09-23 16:28:09 +0530

    Add constants and literals example

---
+++ basics/constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const int MAX_USERS = 100;
+    const float PI = 3.14159f;
+    const char GRADE = 'A';
+
+    printf("Maximum users: %d\n", MAX_USERS);
+    printf("PI: %.5f\n", PI);
+    printf("Grade: %c\n", GRADE);
+
+    return 0;
+}
