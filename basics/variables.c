29bcab0caf0e593f6663543ed6af276b2ca688a2
raviteja vaidya <vydyatej@gmail.com>
2026-09-23 16:23:20 +0530

    Add variables example and gitignore

---
+++ basics/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 27;
+    float temperature = 25.5f;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Temperature: %.1f\n", temperature);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
